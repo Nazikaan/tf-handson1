1b36be9838a690afb5cc6d4ddab5f1efc3bbeca5
Nazikaan <120547919+Nazikaan@users.noreply.github.com>
2024-03-08 14:29:51 -0600

    Update backend.tf
    updated

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     region         = "us-east-1"
     bucket         = "hw1-terraform"
-    key            = "lesson3"
+    key            = "3"
     dynamodb_table = "terraform-state-lock-dynamo"
   }
 }
